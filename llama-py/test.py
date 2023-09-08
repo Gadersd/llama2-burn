65758c2b18939b5aa4f103c8640444b990c68a71
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-09-08 12:28:11 +0200

    fixed load model llama-py

--- llama-py/test.py
+++ llama-py/test.py
@@ -11,10 +11,12 @@ import tokenizer
 def load_model(model_dir, tokenizer_path):
     tok = tokenizer.Tokenizer(model_path=tokenizer_path)
     checkpoints = sorted(Path(model_dir).glob("*.safetensors"))
-    if len(checkpoints) == 0:
-        raise ValueError(f"No checkpoint files found in {model_dir}")
-    
-    weights = [load_file(filename, device="cpu") for filename in checkpoints]
+    if len(checkpoints) > 0:
+        weights = [load_file(filename, device="cpu") for filename in checkpoints]
+        weights = concat_weights(weights)
+    else:
+        weights = torch.load(Path(model_dir) / "consolidated.00.pth")
+
     with open(Path(model_dir) / "params.json", "r") as f:
         params = json.loads(f.read())
     
@@ -24,7 +26,7 @@ def load_model(model_dir, tokenizer_path):
     )
     model_args.vocab_size = tok.n_words
     model = Transformer(model_args)
-    model.load_state_dict(concat_weights(weights), strict=False)
+    model.load_state_dict(weights, strict=False)
     model.max_seq_len = model.tok_embeddings.weight.shape[0]
     print('Loaded model')
 
@@ -52,24 +54,21 @@ if __name__ == "__main__":
     model_dir = sys.argv[1]
     tokenizer_path = sys.argv[2]
 
-    try:
-        with torch.no_grad():
-          tok = tokenizer.Tokenizer(model_path=tokenizer_path)
-          llama = load_model(model_dir, tokenizer_path)
+    with torch.no_grad():
+      tok = tokenizer.Tokenizer(model_path=tokenizer_path)
+      llama = load_model(model_dir, tokenizer_path)
 
-          '''tokens = torch.tensor([0, 2, 1])
-          out = llama(tokens.unsqueeze(0), 0)
+      '''tokens = torch.tensor([0, 2, 1])
+      out = llama(tokens.unsqueeze(0), 0)
 
-          print(out[0, :3, :10].numpy())'''
+      print(out[0, :3, :10].numpy())'''
 
-          tokens = tok.encode("Hello, I am ", True, False)
-          for i in range(0, 10):
-             token_tensor = torch.tensor(tokens)
-             logits = llama(token_tensor.unsqueeze(0), 0)
-             sample = logits[:, -1, :].argmax(dim=-1).item()
-             print(f'Sample is {sample} {tok.decode(sample)}')
-             tokens = tokens + [sample]
-        decoded = tok.decode(tokens)
-        print(f"Sampled output: {decoded}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+      tokens = tok.encode("Hello, I am ", True, False)
+      for i in range(0, 10):
+          token_tensor = torch.tensor(tokens)
+          logits = llama(token_tensor.unsqueeze(0), 0)
+          sample = logits[:, -1, :].argmax(dim=-1).item()
+          print(f'Sample is {sample} {tok.decode(sample)}')
+          tokens = tokens + [sample]
+    decoded = tok.decode(tokens)
+    print(f"Sampled output: {decoded}")
